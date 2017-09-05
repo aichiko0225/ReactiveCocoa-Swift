ab8e6b3d8668e72b848042580154252e0fc72314
aichiko0225 <aichiko66@163.com>
2017-09-05 23:42:25 +0800

    swift

--- ReactiveCocoa-Swift/ViewController.swift
+++ ReactiveCocoa-Swift/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var changeButton: UIButton!
+    
+    var name = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(nameLabel.reactive.text)
+        
+        let (signal, observer) = Signal<String, NSError>.pipe()
+        
+        signal.map { (text) -> Bool in
+            return text.characters.count > 0
+        }.observeCompleted {
+            print("observeCompleted")
+        }
+        
+        observer.sendCompleted()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func change(_ sender: Any) {
+        
+    }
 
 }
 
